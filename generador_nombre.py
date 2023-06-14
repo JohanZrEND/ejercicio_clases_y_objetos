ad86280fc821652f0c530fbcd06c6e46dc377c61
JohanZr <johanzr>
2023-06-14 11:02:42 +0200

    se genera la clase generador nombre y se genera nombre y correo

--- generador_nombre.py
+++ generador_nombre.py
@@ -1,8 +1,34 @@
-from requestexample import nombre, correo
+import requests
 
 class GeneradorDeNombre:
 
     def __init__(self):
-        self.nombre = nombre()
-        self.turno = "A"
-        self.correo = correo()
+        my_data_user = self.generate_name_and_email()
+        self.name = my_data_user["fullname"]
+        self.email = my_data_user["email"]
+
+    def get_name(self):
+        return self.name
+    
+    def get_email(self):
+        return self.email
+
+    def generate_name_and_email(self):
+        results = "1"  # input("Cuantos quieres? ")
+        uri = f'https://randomuser.me/api/?results={results}'
+        r = requests.get(uri)
+        randomuser = r.json()
+        user = randomuser["results"][0]
+        
+        return {
+            "fullname" :f'{user["name"]["first"]} {user["name"]["last"]}',
+            "email" : user["email"]
+        }
+
+if __name__ == "__main__":
+    gen = GeneradorDeNombre()
+    my_data_user = gen.generate_name_and_email()
+    print(my_data_user["fullname"])
+    print(my_data_user["email"])
+    print()
+    print()
