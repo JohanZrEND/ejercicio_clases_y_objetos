28e28135aff5bcb42918c8cf886eb33fc39eee4b
JohanZr <johanzr>
2023-06-16 13:26:50 +0200

    se añade notas como none y el turno

--- alumno.py
+++ alumno.py
@@ -5,14 +5,20 @@ class Alumno(User):
     def __init__(self):
         print("randomuser...")
         super().__init__()
+        self.nota = None
+        self.turno = "A"
     
-
     def setNota(self, nota = 0):
         self.nota = nota
 
     def convocar_examen(self):
-            if self.nota >= 5:
-                print(f"{self.correo} - {self.nombre} - CONVOCADO ☻")
+        if self.nota and self.nota >= 5:
+            print(f"{self.correo} - {self.nombre} - CONVOCADO ☻")
 
     def describe(self):
         print(f"{self.nombre} - {self.turno} - {self.correo} -> Nota : {self.nota}")
+
+if __name__ == "__main__":
+    alumno = Alumno()
+    alumno.setNota(6.12)
+    print(alumno)
